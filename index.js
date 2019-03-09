9fc3f46bc1dc20300f462e01f68ecd25c3f246a0
Farah Student <farah.gus@hotmail.com>
2019-03-09 16:07:03 -0500

    added url

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 'use strict';
 /*global $*/
 
-/*========================================================================================
- REQUIREMENTS:
-The user must be able to search for a GitHub user handle.
-The search must trigger a call to GitHub's API.
-The repos associated with that handle must be displayed on the page.
-You must display the repo name and link to the repo URL.
-The user must be able to make multiple searches and see only the results for the current search.
-========================================================================================*/
-
 function formatQueryParams(params){
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
